56a0646ad35bb3907eba1169b167efa6be770a72
sudhachitral <167319015+sudhachitral@users.noreply.github.com>
2025-08-01 14:20:22 +0530

    is_odd_checker.py

---
+++ is_odd_checker.py
@@ -0,0 +1,7 @@
+#Write a function that returns 2 if the input number is odd (excluding zero), else returns 1.
+def is_odd(n):
+    if n%2!=0:
+        return 2
+    else:
+        return 1
+is_odd(9)
