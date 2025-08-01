115f9d5548b0330042f08f59c392b56dbe89f181
sudhachitral <167319015+sudhachitral@users.noreply.github.com>
2025-08-01 14:18:09 +0530

    is_even_checker.py

---
+++ is_even_checker.py
@@ -0,0 +1,7 @@
+#Write a function that returns 2 if the input number (positive, negative, or zero) is even, otherwise returns 1.
+def is_even(n):
+    if n%2==0:
+        return 2
+    else:
+        return 1
+    
