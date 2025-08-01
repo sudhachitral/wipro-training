addcef8905b167676d8f5e480d4e5d0b83828cbb
sudhachitral <167319015+sudhachitral@users.noreply.github.com>
2025-08-01 14:46:10 +0530

    Create second_last_digit.py

---
+++ second_last_digit.py
@@ -0,0 +1,6 @@
+#Write a function that returns the second last digit (tens place) of a number as a positive integer, or -1 if the number has only one digit.
+
+
+a = -197
+a = str(abs(a))      
+print(int(a[-2])) 
